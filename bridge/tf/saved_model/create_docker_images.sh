88a41fd3d7921f2e741fb7df7a43bddd043171a2
Norbert Schultz <norbert.schultz@ambrosys.de>
2019-08-12 19:15:03 +0000

    Build tf saved model bridge inside Docker

--- bridge/tf/saved_model/create_docker_images.sh
+++ bridge/tf/saved_model/create_docker_images.sh
@@ -6,6 +6,15 @@ MYDIR=`dirname $0`
 cd $MYDIR
 echo "MYDIR $PWD"
 
+# We can't use the local build executable, as we need Tensorflow C-Libraries
+# and they are not buildable on Mac for Linux.
+# So we are rebuilding on docker
+
+# Step 1 Preparing a Build directory
+rm -rf docker_build
+mkdir -p docker_build
+cp -r ../../../go_shared docker_build
+
 . ../../../scripts/ci/docker_help.sh
 
 $DOCKER_CALL build -t bridge.tf.saved_model .
